a034104bbf96b3d293bca4ae371848e15b5e1529
ian-ludden <iludden94@gmail.com>
2021-05-18 14:48:23 -0500

    Started creating 3-D geo-graph class.

---
+++ src/3dgeograph.hh
@@ -0,0 +1,14 @@
+/** 
+ * \file 3dgeograph.hh
+ * \brief Header file for 3dgeograph. */
+
+#ifndef THREE_DEE_GEOGRAPH_HH
+#define THREE_DEE_GEOGRAPH_HH
+
+#include <vector>
+
+namespace tdgg {
+    
+}
+
+#endif

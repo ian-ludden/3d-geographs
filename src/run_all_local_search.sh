97cd8396480e0524f1c5689a57b29429a793d00f
ian-ludden <iludden94@gmail.com>
2021-09-30 11:27:06 -0500

    Small change to shell script for running experiments.

--- src/run_all_local_search.sh
+++ src/run_all_local_search.sh
@@ -7,19 +7,19 @@ K=8
 
 for fname in ../generate_input/grid/* 
 do
-	echo $fname >> local_search_grid.out
-	echo "10k flips,K=8,attempt_flip" >> local_search_grid.out
-	./local_search.exe $fname $K >> local_search_grid.out
-	echo "10k flips,K=8,attempt_flip_BFS" >> local_search_grid.out
-	./local_search_BFS.exe $fname $K >> local_search_grid.out
+	echo $fname >> ../results/local_search_grid.out
+	echo "10k flips,K=8,attempt_flip" >> ../results/local_search_grid.out
+	./local_search.exe $fname $K >> ../results/local_search_grid.out
+	echo "10k flips,K=8,attempt_flip_BFS" >> ../results/local_search_grid.out
+	./local_search_BFS.exe $fname $K >> ../results/local_search_grid.out
 	
 done
 
 for fname in ../generate_input/bcc/* 
 do
-	echo $fname >> local_search_bcc.out
-	echo "10k flips,K=8,attempt_flip" >> local_search_bcc.out
-	./local_search.exe $fname $K >> local_search_bcc.out
-	echo "10k flips,K=8,attempt_flip_BFS" >> local_search_bcc.out
-	./local_search_BFS.exe $fname $K >> local_search_bcc.out
+	echo $fname >> ../results/local_search_bcc.out
+	echo "10k flips,K=8,attempt_flip" >> ../results/local_search_bcc.out
+	./local_search.exe $fname $K >> ../results/local_search_bcc.out
+	echo "10k flips,K=8,attempt_flip_BFS" >> ../results/local_search_bcc.out
+	./local_search_BFS.exe $fname $K >> ../results/local_search_bcc.out
 done
